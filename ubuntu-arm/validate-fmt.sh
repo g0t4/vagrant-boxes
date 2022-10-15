8a607c90200a00f6858d329f5b87c34a0ec85010
Wes Higbee <wes.mcclure@gmail.com>
2022-10-15 16:41:19 -0500

    fix fmt to work on nested files

--- ubuntu-arm/validate-fmt.sh
+++ ubuntu-arm/validate-fmt.sh
@@ -1,16 +1,9 @@
 
-packer validate \
-  -var-file="2204/pkrvars.hcl" \
-  .
+# validate both builds
+packer validate -var-file="2204/pkrvars.hcl" .
+packer validate -var-file="2210/pkrvars.hcl" .
 
-packer validate \
-  -var-file="2210/pkrvars.hcl" \
-  .
-
-packer fmt \
-  -var-file="2204/pkrvars.hcl" \
-  .
-
-packer fmt \
-  -var-file="2210/pkrvars.hcl" \
-  .
+# fmt all files
+packer fmt .
+packer fmt 2204/pkrvars.hcl
+packer fmt 2210/pkrvars.hcl
