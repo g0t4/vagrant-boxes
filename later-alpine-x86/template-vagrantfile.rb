7a57cbea197072a9e09b331804efb41f494a181d
Wes Higbee <wes.mcclure@gmail.com>
2022-10-21 02:27:19 -0500

    notes

--- later-alpine-x86/template-vagrantfile.rb
+++ later-alpine-x86/template-vagrantfile.rb
@@ -8,6 +8,7 @@ Vagrant.configure(2) do |config|
 
   config.vm.provider :virtualbox do |v, override|
     v.gui = false
+    # modifyvm docs: https://docs.oracle.com/en/virtualization/virtualbox/7.0/user/vboxmanage.html
     v.customize ["modifyvm", :id, "--vram", 256]
     v.customize ["modifyvm", :id, "--cpus", 2]
     v.customize ["modifyvm", :id, "--memory", 2048]
