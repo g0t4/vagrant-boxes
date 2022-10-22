562e5f477312a83f880439b75dd523b56b54a33f
Wes Higbee <wes.mcclure@gmail.com>
2022-10-22 14:06:05 -0500

    split apart commands for legibility - this is why a script is superior - not sure why part is in script and part isn't, this takes forever to type too into the VM when building it versus just download it - like it was already downloading the .cfg script!

--- later-alpine-x86/http/boot_command.sh
+++ later-alpine-x86/http/boot_command.sh
@@ -24,12 +24,18 @@ export DISKOPTS="-s 0 -m sys /dev/sda"
 # END inlined
 
 printf "vagrant\nvagrant\ny\n" \
-  | sh /sbin/setup-alpine -f /root/generic.alpine316.vagrant.cfg \
-  && mount /dev/sda2 /mnt \
-  && sed -E -i '/#? ?PasswordAuthentication/d' /mnt/etc/ssh/sshd_config \
-  && sed -E -i '/#? ?PermitRootLogin/d' /mnt/etc/ssh/sshd_config \
-  && echo 'PasswordAuthentication yes' >> /mnt/etc/ssh/sshd_config \
-  && echo 'PermitRootLogin yes' >> /mnt/etc/ssh/sshd_config \
-  && chroot /mnt apk add openntpd \
-  && chroot /mnt rc-update add openntpd default \
-  && reboot
+  | sh /sbin/setup-alpine -f /root/generic.alpine316.vagrant.cfg
+
+mount /dev/sda2 /mnt
+
+# todo escaping?
+sed -E -i '/#? ?PasswordAuthentication/d' /mnt/etc/ssh/sshd_config
+sed -E -i '/#? ?PermitRootLogin/d' /mnt/etc/ssh/sshd_config
+
+echo 'PasswordAuthentication yes' >> /mnt/etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /mnt/etc/ssh/sshd_config
+
+chroot /mnt apk add openntpd
+chroot /mnt rc-update add openntpd default
+
+reboot
