2fb72ad805fba91e9eef20ef414cc601ba49820d
Wes Higbee <wes.mcclure@gmail.com>
2024-01-27 16:08:55 -0600

    2310

--- ubuntu-arm/validate-fmt.sh
+++ ubuntu-arm/validate-fmt.sh
@@ -1,9 +1,9 @@
 
 # validate both builds
 packer validate -var-file="2204/pkrvars.hcl" .
-packer validate -var-file="2210/pkrvars.hcl" .
+packer validate -var-file="2310/pkrvars.hcl" .
 
 # fmt all files
 packer fmt .
 packer fmt 2204/pkrvars.hcl
-packer fmt 2210/pkrvars.hcl
+packer fmt 2310/pkrvars.hcl
