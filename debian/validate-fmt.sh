d3ab34113391dd4fe042280416b124acbf67f7d8
Wes Higbee <wes.mcclure@gmail.com>
2024-02-25 17:19:35 -0600

    setup debian 12 build

--- debian/validate-fmt.sh
+++ debian/validate-fmt.sh
@@ -3,10 +3,8 @@
 vagrant_cloud_token
 
 # validate both builds
-packer validate -var-file="2204/pkrvars.hcl" .
-packer validate -var-file="2310/pkrvars.hcl" .
+packer validate -var-file="12/pkrvars.hcl" .
 
 # fmt all files
 packer fmt .
-packer fmt 2204/pkrvars.hcl
-packer fmt 2310/pkrvars.hcl
+packer fmt 12/pkrvars.hcl
