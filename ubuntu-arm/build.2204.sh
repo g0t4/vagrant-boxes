4507d868faa1119d1191a5c9aa659c4fe808f0b9
Wes Higbee <wes.mcclure@gmail.com>
2024-02-24 23:15:51 -0600

    nevermind lets use Current

--- ubuntu-arm/build.2204.sh
+++ ubuntu-arm/build.2204.sh
@@ -1,5 +1,5 @@
 # temp workaround for parallels virtualization sdk
-export PYTHONPATH=/Library/Frameworks/ParallelsVirtualizationSDK.framework/Versions/11/Libraries/Python/3.7
+export PYTHONPATH=/Library/Frameworks/ParallelsVirtualizationSDK.framework/Versions/Current/Libraries/Python/3.7
 
 packer build -on-error ask -timestamp-ui \
   -var-file="2204/pkrvars.hcl" \
