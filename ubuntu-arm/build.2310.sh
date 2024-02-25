ff5dad01480f75275d252f9826b2a204f89cec4d
Wes Higbee <wes.mcclure@gmail.com>
2024-02-24 23:15:22 -0600

    bump to v11

--- ubuntu-arm/build.2310.sh
+++ ubuntu-arm/build.2310.sh
@@ -1,5 +1,5 @@
 # temp workaround for parallels virtualization sdk
-export PYTHONPATH=/Library/Frameworks/ParallelsVirtualizationSDK.framework/Versions/10/Libraries/Python/3.7
+export PYTHONPATH=/Library/Frameworks/ParallelsVirtualizationSDK.framework/Versions/11/Libraries/Python/3.7
 
 packer build -on-error ask -timestamp-ui \
   -var-file="2310/pkrvars.hcl" \
