8f13ba8822a46194176237ade7bc0ea1efe77e9b
Wes Higbee <wes.mcclure@gmail.com>
2024-02-26 10:52:51 -0600

    sync apt from debian to ubuntu

--- ubuntu-arm/scripts/apt.sh
+++ ubuntu-arm/scripts/apt.sh
@@ -1,5 +1,40 @@
 #!/bin/sh -eux
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
+# sync with debian/scripts/apt.sh
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
+# avoid deadlock during install:
+sudo systemctl stop \
+  apt-daily.timer \
+  apt-daily-upgrade.timer \
+  apt-daily.service \
+  apt-daily-upgrade.service
+
+# disable apt timers/services so they don't run on VMs booted from this box too:
+sudo systemctl disable \
+  apt-daily.timer \
+  apt-daily-upgrade.timer \
+  apt-daily.service \
+  apt-daily-upgrade.service
+sudo systemctl mask \
+  apt-daily.service \
+  apt-daily-upgrade.service \
+  apt-daily-upgrade.timer \
+  apt-daily.timer
+sudo systemctl daemon-reload
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get autoremove -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get autoclean -y
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+  apt-file \
+  command-not-found \
+  fish \
+  grc \
+  lsof \
+  psmisc \
+  silversearcher-ag \
+  tree \
+  util-linux \
+  vim
