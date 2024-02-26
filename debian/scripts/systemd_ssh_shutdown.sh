1d242f4f067a1d16df83900477d9655368566096
Wes Higbee <wes.mcclure@gmail.com>
2024-02-25 23:50:01 -0600

    notes

--- debian/scripts/systemd_ssh_shutdown.sh
+++ debian/scripts/systemd_ssh_shutdown.sh
@@ -5,3 +5,5 @@
 
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=751636
 apt-get install libpam-systemd
+
+# I had a hang on ssh when packer cleaned up the VM (I was ssh'd in) so this may be an issue, that said using systemctl shutdown may not trigger the hang as it didn't do it the time before when I manually did it that way
