f08fd3383dfeaf3727abfa2fba3b99028c3c9a1d
Wes Higbee <wes.mcclure@gmail.com>
2024-02-25 22:49:53 -0600

    review manually

--- debian/scripts/updates.sh
+++ debian/scripts/updates.sh
@@ -34,13 +34,10 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get autoremove -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get autoclean -y
 
-# TODO update kernel+headers?
-# arch="$(uname -r | sed 's/^.*[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\(-[0-9]\{1,2\}\)-//')"
-# apt-get -y upgrade linux-image-"$arch";
-# apt-get -y install linux-headers-"$(uname -r)";
-
 # TODO add any packages I want on all instances of this box:
 #  mlocate? (review config for this in bento too)
 
 # Notes w.r.t. bento/robox:
 # - cdrom in /etc/apt/sources.list is commented out so don't worry about it
+# - headers/kernel are both installed and upgraded b/c dist-upgrade (robox didn't do dist-upgrade and thus upgrades/installs kernel/headers)
+
