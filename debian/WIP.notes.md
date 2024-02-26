cf45caefcd6da346fbb9f4955ce1469880ed0b70
Wes Higbee <wes.mcclure@gmail.com>
2024-02-25 22:28:58 -0600

    notes

--- debian/WIP.notes.md
+++ debian/WIP.notes.md
@@ -6,17 +6,3 @@ NEXT UP need to add/review scripts and that should be about it!!
     FYI bento uses (AFICT for debian):
       echo 'vagrant' | sudo -S /sbin/halt -h -p
       # I don't think I need to pass the password, the first shutdown is after passwordless sudo is setup
-
-### bento script review
-
-
-- network.sh
-  - PRN disable ipv6 if have build issues, i.e. vagrant tty bug can be related to ipv6... but lets not do this if its not an issue for me
-    - https://github.com/lavabit/robox/blob/eadba7cd7a3aa58e6f6f2f3e92fc51585ab2828b/scripts/debian12/network.sh#L34-L40
-  - set hostname?
-    - https://github.com/lavabit/robox/blob/eadba7cd7a3aa58e6f6f2f3e92fc51585ab2828b/scripts/debian12/network.sh#L43-L45
-  - 
-
-### PRN robox script review
-
-- https://github.com/lavabit/robox/blob/master/scripts/debian12/apt.sh
--- debian/scripts/networking.sh
+++ debian/scripts/networking.sh
@@ -1,11 +1,16 @@
+# PRN disable ipv6 if have build issues, i.e. vagrant tty bug can be related to ipv6... but lets not do this if its not an issue for me
+#   PRN run before apt update? maybe => FYI maybe just combine all these setup scripts so it is obvious the order
+# https://github.com/lavabit/robox/blob/eadba7cd7a3aa58e6f6f2f3e92fc51585ab2828b/scripts/debian12/network.sh#L34-L40
+
+
+# set hostname?
+# https://github.com/lavabit/robox/blob/eadba7cd7a3aa58e6f6f2f3e92fc51585ab2828b/scripts/debian12/network.sh#L43-L45
 
 # PRN add delay? both bento and robox have this:
 # printf "pre-up sleep 2\n" >> /etc/network/interfaces
 
 # bento/networking_debian.sh
 # PRN (if issues): don't disable predictable network interface naming, esp b/c I expect VMs to have multiple nics so eth0 alone isn't gonna cut it
-
-# set hostname
 
 # set nameservers?
 
