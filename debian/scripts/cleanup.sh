6a26e2fc21bf83aed6d3b391b5246a54e28d9a3d
Wes Higbee <wes.mcclure@gmail.com>
2024-02-25 23:46:27 -0600

    fix

--- debian/scripts/cleanup.sh
+++ debian/scripts/cleanup.sh
@@ -2,9 +2,9 @@
 
 # *** from https://github.com/chef/bento/blob/45792040a997156a257660bd3e2d8f6b1209b677/packer_templates/scripts/debian/cleanup_debian.sh#L50-L56
 echo "blank netplan machine-id (DUID) so machines get unique ID generated on boot"
-truncate -s 0 /etc/machine-id
+sudo truncate -s 0 /etc/machine-id
 if test -f /var/lib/dbus/machine-id; then
-  truncate -s 0 /var/lib/dbus/machine-id  # if not symlinked to "/etc/machine-id"
+  sudo truncate -s 0 /var/lib/dbus/machine-id  # if not symlinked to "/etc/machine-id"
 fi
 
 # Remove the random seed so a unique value is used the first time the box is booted.
