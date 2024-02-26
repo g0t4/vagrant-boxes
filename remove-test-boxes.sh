0916be242feb9dbe2b029e3e54ce52414ac22770
Wes Higbee <wes.mcclure@gmail.com>
2024-02-26 13:37:30 -0600

    remove

--- remove-test-boxes.sh
+++ remove-test-boxes.sh
@@ -1,4 +1,5 @@
 vagrant box remove test-ubuntu2204-arm
 vagrant box remove test-ubuntu2310-arm
+vagrant box remove test-ubuntu2404-arm
 vagrant box remove test-centos9s-arm
 vagrant box remove test-debian12-arm
