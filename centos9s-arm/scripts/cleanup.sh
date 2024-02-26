2ecb97c00802612c361fbe6b222acfcca15ca24e
Wes Higbee <wes.mcclure@gmail.com>
2024-02-26 11:20:17 -0600

    sync cleanup

--- centos9s-arm/scripts/cleanup.sh
+++ centos9s-arm/scripts/cleanup.sh
@@ -8,5 +8,9 @@ if [ -f /etc/sysconfig/network-scripts/ifcfg-eth0 ]; then
   sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
 fi
 
-# Clear the random seed.
-rm -f /var/lib/systemd/random-seed
+# Remove the random seed so a unique value is used the first time the box is booted.
+sudo systemctl --quiet is-active systemd-random-seed.service
+sudo systemctl stop systemd-random-seed.service
+[ -f /var/lib/systemd/random-seed ] && sudo rm --force /var/lib/systemd/random-seed
+
+# todo /etc/machine-id
