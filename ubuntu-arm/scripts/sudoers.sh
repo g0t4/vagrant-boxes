0863fc1dbf5dd5f6c5db3b3271c8053ca6df7cd8
Wes Higbee <wes.mcclure@gmail.com>
2024-02-26 10:55:33 -0600

    mirror

--- ubuntu-arm/scripts/sudoers.sh
+++ ubuntu-arm/scripts/sudoers.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -eux
 
-# this is run with elevated permissions (obviously needed)
-
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 
 # Set up password-less sudo for the vagrant user
