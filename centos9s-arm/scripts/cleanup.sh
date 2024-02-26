a67e41cdf90fbf54774140cade096f590b15dfba
Wes Higbee <wes.mcclure@gmail.com>
2024-02-26 11:21:32 -0600

    /etc/machine-id

--- centos9s-arm/scripts/cleanup.sh
+++ centos9s-arm/scripts/cleanup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -eux
 
-# from https://github.com/lavabit/robox/blob/master/scripts/centos9s/cleanup.sh
+# sync with debian/scripts/cleanup.sh
 
+# from https://github.com/lavabit/robox/blob/eadba7cd7a3aa58e6f6f2f3e92fc51585ab2828b/scripts/centos9s/cleanup.sh#L5-L9
 echo "Remove the ethernet identity values.\n"
 if [ -f /etc/sysconfig/network-scripts/ifcfg-eth0 ]; then
   sed -i /UUID/d /etc/sysconfig/network-scripts/ifcfg-eth0
   sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
+fi
+
+# *** from https://github.com/chef/bento/blob/45792040a997156a257660bd3e2d8f6b1209b677/packer_templates/scripts/debian/cleanup_debian.sh#L50-L56
+echo "blank netplan machine-id (DUID) so machines get unique ID generated on boot"
+sudo truncate -s 0 /etc/machine-id
+if test -f /var/lib/dbus/machine-id; then
+  sudo truncate -s 0 /var/lib/dbus/machine-id  # if not symlinked to "/etc/machine-id"
 fi
 
 # Remove the random seed so a unique value is used the first time the box is booted.
