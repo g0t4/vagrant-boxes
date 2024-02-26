6e07399737800f39df737586077c8136efce0c54
Wes Higbee <wes.mcclure@gmail.com>
2024-02-26 11:45:47 -0600

    review centos package

--- centos9s-arm/scripts/apt.sh
+++ centos9s-arm/scripts/apt.sh
@@ -2,12 +2,40 @@
 
 dnf --assumeyes update
 
-# already installed
-dnf --assumeyes install dmidecode coreutils grep gawk sed curl \
-  psmisc libarchive gcc-c++ libstdc++-devel cpp glibc-devel
+# packages to install (include already installed, not worth it to double check each release)
+dnf --assumeyes install \
+  bash-completion \
+  mlocate \
+  rsync \
+  vim-enhanced \
+  bind-utils \
+  wget \
+  dos2unix unix2dos \
+  lsof \
+  telnet \
+  net-tools \
+  patch \
+  sysstat \
+  make \
+  cmake \
+  autoconf \
+  automake \
+  libtool \
+  ncurses-devel \
+  glibc-headers
 
-# not already installed
-dnf --assumeyes install bash-completion mlocate \
-  rsync vim-enhanced bind-utils wget dos2unix unix2dos lsof telnet \
-  net-tools patch sysstat make cmake autoconf automake libtool \
-  ncurses-devel glibc-headers
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+  apt-file \
+  bash-completion \
+  command-not-found \
+  curl wget \
+  fish \
+  grc \
+  iproute2 \
+  lsof \
+  procps \
+  psmisc \
+  silversearcher-ag \
+  tree \
+  util-linux \
+  vim
