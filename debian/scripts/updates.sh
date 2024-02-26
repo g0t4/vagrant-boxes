c4b39b27b8a5e1db5233a1f0be5f9f807480481d
Wes Higbee <wes.mcclure@gmail.com>
2024-02-25 22:46:29 -0600

    derp wes

--- debian/scripts/updates.sh
+++ debian/scripts/updates.sh
@@ -4,38 +4,35 @@
 # bento: https://github.com/chef/bento/blob/master/packer_templates/scripts/debian/update_debian.sh
 # robox: https://github.com/lavabit/robox/blob/master/scripts/debian12/apt.sh
 
-export DEBIAN_FRONTEND=noninteractive
+export
 export DEBCONF_NONINTERACTIVE_SEEN=true
 
 # avoid deadlock during install:
-systemctl stop \
+sudo systemctl stop \
   apt-daily.timer \
   apt-daily-upgrade.timer \
   apt-daily.service \
   apt-daily-upgrade.service
 # TODO do I really wanna disable apt timers/services for VMs booted from this box?
 # disable apt timers/services so they don't run on VMs booted from this box too:
-systemctl disable \
+sudo systemctl disable \
   apt-daily.timer \
   apt-daily-upgrade.timer \
   apt-daily.service \
   apt-daily-upgrade.service
-systemctl mask \
+sudo systemctl mask \
   apt-daily.service \
   apt-daily-upgrade.service \
   apt-daily-upgrade.timer \
   apt-daily.timer
-systemctl daemon-reload
+sudo systemctl daemon-reload
 # FYI don't modify default cleanup config, that way if someone wants to enable it, it's still there:
 #  /etc/apt/apt.conf.d/10periodic
 
-# TODO robox has remove cdrom as a media source, not sure I wanna get rid of this, esp if I switch to using DVD iso that presumably would have many packages avail locally (likely with newest version if downloaded recently)...
-# sed -i -e "/cdrom:/d" /etc/apt/sources.list
-
-apt-get update
-apt-get dist-upgrade -y
-apt-get autoremove -y
-apt-get autoclean -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get autoremove -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get autoclean -y
 
 # TODO update kernel+headers?
 # arch="$(uname -r | sed 's/^.*[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\(-[0-9]\{1,2\}\)-//')"
@@ -44,3 +41,6 @@ apt-get autoclean -y
 
 # TODO add any packages I want on all instances of this box:
 #  mlocate? (review config for this in bento too)
+
+# Notes w.r.t. bento/robox:
+# - cdrom in /etc/apt/sources.list is commented out so don't worry about it
