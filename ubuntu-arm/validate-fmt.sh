ba1e8bce7b9b79879445e7d452d274d0924b14d3
Wes Higbee <wes.mcclure@gmail.com>
2025-04-02 18:35:42 -0500

    2504 and others later

--- ubuntu-arm/validate-fmt.sh
+++ ubuntu-arm/validate-fmt.sh
@@ -3,6 +3,7 @@
 vagrant_cloud_token
 
 # validate both builds
+# TODO bring back other builds (do it as newer builds, i.e. 2504)
 # packer validate -var-file="2204/pkrvars.hcl" .
 # packer validate -var-file="2410/pkrvars.hcl" .
 packer validate -var-file="2404/pkrvars.hcl" \
@@ -12,6 +13,7 @@ packer validate -var-file="2404/pkrvars.hcl" \
 
 # fmt all files
 packer fmt .
-packer fmt 2204/pkrvars.hcl
-packer fmt 2410/pkrvars.hcl
+# TODO bring back other builds
+# packer fmt 2204/pkrvars.hcl
+# packer fmt 2410/pkrvars.hcl
 packer fmt 2404/pkrvars.hcl
