a8f14fd4920adb95124906c0b41c4e50e210a8b8
Wes Higbee <wes.mcclure@gmail.com>
2025-04-02 18:33:07 -0500

    add vars in validate and also fish shell script no ${}

--- ubuntu-arm/validate-fmt.sh
+++ ubuntu-arm/validate-fmt.sh
@@ -3,9 +3,12 @@
 vagrant_cloud_token
 
 # validate both builds
-packer validate -var-file="2204/pkrvars.hcl" .
-packer validate -var-file="2410/pkrvars.hcl" .
-packer validate -var-file="2404/pkrvars.hcl" .
+# packer validate -var-file="2204/pkrvars.hcl" .
+# packer validate -var-file="2410/pkrvars.hcl" .
+packer validate -var-file="2404/pkrvars.hcl" \
+    -var="client_id=$VAGRANT_HCP_CLIENT_ID" \
+    -var="client_secret=$VAGRANT_HCP_CLIENT_SECRET" \
+    .
 
 # fmt all files
 packer fmt .
