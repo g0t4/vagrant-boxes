da448752088094c53a3a4f413c2d2d3f93f3a188
Wes Higbee <wes.mcclure@gmail.com>
2026-03-30 17:02:33 -0500

    more packges OOB

--- arch-arm/scripts/pacman.sh
+++ arch-arm/scripts/pacman.sh
@@ -5,20 +5,38 @@ sudo pacman -Syu --noconfirm
 
 # Install packages
 sudo pacman -S --noconfirm --needed \
+  bat \
   bash-completion \
   curl \
+  dust \
+  fzf \
   fish \
   git \
+  git-delta \
+  git-lfs \
   grc \
+  httpie \
+  hwinfo \
+  jq \
+  less \
+  lshw \
+  most \
+  neovim \
+  nmap \
   iproute2 \
   lsof \
   procps-ng \
   psmisc \
-  the_silver_searcher \
+  ripgrep \
+  strace \
   tree \
+  trash-cli \
+  unzip \
+  uv \
   util-linux \
   vim \
-  wget
+  wget \
+  which
 
 # Clean package cache
 sudo pacman -Scc --noconfirm
